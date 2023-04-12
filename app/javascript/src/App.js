2c6350c9861a7e692ed4f3b94c2a5151fa04daea
Boniface Mwenda <bonny.mwenda04@gmail.com>
2023-04-13 02:57:11 +0300

    Initial commit  - set up project with rails 6  - configure react  - configure docker

---
+++ app/javascript/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import './App.css';
+
+function HelloMessage({ name }) {
+  return (
+    <h1>
+      Greetings,
+      {name}
+      !
+    </h1>
+  );
+}
+export default HelloMessage;
