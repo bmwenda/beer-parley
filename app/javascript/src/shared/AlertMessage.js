85d132904b4d4ea86c334d675a53f91f05459dc8
Boniface Mwenda <bonny.mwenda04@gmail.com>
2023-04-17 22:38:53 +0300

    Refactor alert message

--- app/javascript/src/shared/AlertMessage.js
+++ app/javascript/src/shared/AlertMessage.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Alert, AlertTitle } from '@mui/material';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
+// eslint-disable-next-line react/jsx-props-no-spreading
+const Alert = React.forwardRef((props, ref) => <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />);
 
 export default function AlertMessage({ type, message }) {
+  const [state, setState] = React.useState({
+    open: true,
+    vertical: 'top',
+    horizontal: 'center',
+  });
+
+  const { vertical, horizontal, open } = state;
+
+  const handleClose = () => {
+    setState({ ...state, open: false });
+  };
+
   return (
-    <Alert severity={type} sx={{ justifyContent: 'center', width: '100%' }}>
-      <AlertTitle>{type}</AlertTitle>
-      {message}
-    </Alert>
+    <Snackbar
+      open={open}
+      key={vertical + horizontal}
+      anchorOrigin={{ vertical, horizontal }}
+      autoHideDuration={6000}
+      onClose={handleClose}
+    >
+      <Alert onClose={handleClose} severity={type} sx={{ width: '100%' }}>
+        {message}
+      </Alert>
+    </Snackbar>
   );
 }
 
