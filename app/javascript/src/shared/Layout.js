aaa51e0655f1ecf209f320f3e79ab80e61b9e084
Boniface Mwenda <bonny.mwenda04@gmail.com>
2023-04-18 23:08:58 +0300

    Add navigation menus

--- app/javascript/src/shared/Layout.js
+++ app/javascript/src/shared/Layout.js
@@ -1,38 +1,13 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import SportsBarIcon from '@mui/icons-material/SportsBar';
-import Link from '@mui/material/Link';
-import ProfileMenu from '../components/ProfileMenu';
 import AuthContext from '../contexts/AuthContext';
+import TopBar from './TopBar';
 
 export default function Layout({ children }) {
-  const currentUser = useContext(AuthContext);
+
   return (
     <>
-      <AppBar position="relative">
-        <Toolbar>
-          <Grid container>
-            <Grid item xs={6}>
-              <Link href="/" underline="none" variant="h6" sx={{ color: 'white' }}>
-                <SportsBarIcon sx={{ mr: 2, color: 'white' }} fontSize="large" />
-              </Link>
-            </Grid>
-            <Grid item xs={6}>
-              <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-                {currentUser ? (<ProfileMenu />) : (
-                  <Link href="/login" variant="h6" noWrap>
-                    Login
-                  </Link>
-                )}
-              </Box>
-            </Grid>
-          </Grid>
-        </Toolbar>
-      </AppBar>
+      <TopBar />
       <main>
         {children}
       </main>
