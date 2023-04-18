e8f91b5cbd3a6109ebbf818c024ec39f3bca15c9
Boniface Mwenda <bonny.mwenda04@gmail.com>
2023-04-18 23:25:39 +0300

    Add eslint command

--- app/javascript/src/shared/Layout.js
+++ app/javascript/src/shared/Layout.js
@@ -1,10 +1,8 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import AuthContext from '../contexts/AuthContext';
 import TopBar from './TopBar';
 
 export default function Layout({ children }) {
-
   return (
     <>
       <TopBar />
