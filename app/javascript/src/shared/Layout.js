2e5ff905d89ea7f8887af3a8e13adc5b105c2297
Boniface Mwenda <bonny.mwenda04@gmail.com>
2023-04-18 22:18:44 +0300

    Add profile menu

--- app/javascript/src/shared/Layout.js
+++ app/javascript/src/shared/Layout.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import SportsBarIcon from '@mui/icons-material/SportsBar';
-import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
+import ProfileMenu from '../components/ProfileMenu';
+import AuthContext from '../contexts/AuthContext';
 
 export default function Layout({ children }) {
+  const currentUser = useContext(AuthContext);
   return (
     <>
       <AppBar position="relative">
         <Toolbar>
-          <Link href="/"><SportsBarIcon sx={{ mr: 2, color: 'white' }} /></Link>
-          <Typography variant="h6" color="inherit" noWrap>
-            Beers
-          </Typography>
+          <Grid container>
+            <Grid item xs={6}>
+              <Link href="/" underline="none" variant="h6" sx={{ color: 'white' }}>
+                <SportsBarIcon sx={{ mr: 2, color: 'white' }} fontSize="large" />
+              </Link>
+            </Grid>
+            <Grid item xs={6}>
+              <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                {currentUser ? (<ProfileMenu />) : (
+                  <Link href="/login" variant="h6" noWrap>
+                    Login
+                  </Link>
+                )}
+              </Box>
+            </Grid>
+          </Grid>
         </Toolbar>
       </AppBar>
       <main>
