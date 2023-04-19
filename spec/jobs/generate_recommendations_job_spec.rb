94952cd8d6cc06413e9b1735c6eebc13053d9401
Boniface Mwenda <bonny.mwenda04@gmail.com>
2023-04-20 02:14:36 +0300

    Refactor job specs

--- spec/jobs/generate_recommendations_job_spec.rb
+++ spec/jobs/generate_recommendations_job_spec.rb
@@ -7,17 +7,12 @@ RSpec.describe GenerateRecommendationsJob, type: :job do
     let(:user) { create(:user) }
     let(:job) { described_class.perform_later(user.id) }
 
-    it 'calls beer recommendations module' do
-      expect(Recommendations::BeerRecommendations).to receive(:create_recommendations).with(user)
-      GenerateRecommendationsJob.new.perform(user.id)
-    end
-
     it 'queues the job' do
       expect { job }.to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
     end
 
     it 'rescues exception when user is not found' do
-      expect { GenerateRecommendationsJob.new.perform(100) }.not_to raise_error
+      expect { GenerateRecommendationsJob.perform_later(100) }.not_to raise_error
     end
   end
 end
