2d65322459edab40b3fa86caeb503bb441d6baef
Boniface Mwenda <bonny.mwenda04@gmail.com>
2024-05-16 21:13:11 +0300

    Create welcome email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,11 +33,6 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -73,4 +68,12 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Maiiler configs
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'http://localhost:3000' }
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = { from: 'no-reply@localhost' }
 end
